466c64f654328e70581203d91b8a469ad7827bf1
soundreaper <soundreaper@gmail.com>
2020-07-16 01:19:32 -0700

    add testing file, not returning correct response code

---
+++ tests/server.test.js
@@ -0,0 +1,26 @@
+const app = require("../server.js")
+
+const chai = require("chai")
+const chaiHttp = require('chai-http');
+// const mocha = require('mocha')
+
+// chai.config.includeStack = true
+
+const expect = chai.expect
+// const describe = mocha.describe
+// const it = mocha.it
+chai.use(chaiHttp);
+
+describe('Route Testing', () => {
+
+    // Test Case 1
+    it('Should redirect from / and return 200 response.', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                if (err) done(err);
+                expect(res).to.have.status(200);
+                done();
+            })
+    });
+});
